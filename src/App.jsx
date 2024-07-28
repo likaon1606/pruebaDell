7d3795c8d8199e0a63ce7fab2c4d149c88aca5f0
Ariel Fuentes <ariel.fuentesgarcia@gmail.com>
2024-07-28 00:09:20 -0600

    prueba video

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Video360 from './components/Video360';
 function App() {
   return (
     <div className='App'>
-      <h1>My 360 Video in React</h1>
+      <h1>My 360 Video in React | DELL</h1>
+
       <div style={{ width: '400px', height: '500px' }}>
         <Video360 />
       </div>
